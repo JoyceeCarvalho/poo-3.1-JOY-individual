2a090dd8cc06d78350879257818696a4d2f4d826
Joy Carvalho <94726085+JoyceeCarvalho@users.noreply.github.com>
2023-03-27 22:46:15 -0300

    primeira funcao Java- Joyciane

---
+++ main.java
@@ -0,0 +1,15 @@
+package primeiraFuncaoJava;
+
+public class void primeiraFuncao {
+
+    public void funcao1() {
+        mensagemjava();
+
+    }
+
+    
+    public void mensagemjava() {
+        System.out.println("OLá, essa é a minha primeira funcao java: Testando");
+
+    }
+}
